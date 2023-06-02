9a25a034a809b51faffec7c9137c72445a844ad9
ScorpioXKiller <dima.gorodov1999@gmail.com>
2023-06-02 20:38:15 +0300

    build the project structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    HomeComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
