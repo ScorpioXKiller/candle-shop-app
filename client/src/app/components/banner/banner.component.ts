a3668a5b2922e714e5d58d347f1d90eb7268c837
ScorpioXKiller <dima.gorodov1999@gmail.com>
2023-06-16 09:11:58 +0300

    project build

---
+++ client/src/app/components/banner/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent {
+  private bannerContent: string = 'header.banner.content'
+  private repetitions: number = 3;
+  readonly bannerItems: string[] = Array(this.repetitions).fill(this.bannerContent);
+}
